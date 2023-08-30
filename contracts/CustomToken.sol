595c687370ef1fd212243676caf3cb17381ec95e
Yash Tiwari <yash22012002@gmail.com>
2023-08-30 15:05:29 +0530

    liquidity

--- contracts/CustomToken.sol
+++ contracts/CustomToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract CustomToken is ERC20, Ownable, Pausable {
     using SafeMath for uint256;
@@ -13,12 +14,9 @@ contract CustomToken is ERC20, Ownable, Pausable {
     uint256 public minTransactionAmount = 100 * (10 ** decimals()); // Minimum transaction amount
     uint256 public maxTransactionAmount = 10000 * (10 ** decimals()); // Maximum transaction amount
     uint256 public transferDelay; // Transfer delay period
-
-    mapping(address => bool) private isBlacklisted;
-    mapping(address => uint256) private _transferAllowedAt;
-    mapping(address => bool) private _frozenWallets;
-    mapping(address => bool) private _whitelistedWallets;
-    mapping(address => VestingSchedule) private _vestingSchedules;
+    IUniswapV2Router02 public uniswapRouter;
+    address public liquidityPair;
+    uint256 public requiredSignatures;
 
     struct VestingSchedule {
         uint256 startTimestamp;
@@ -28,14 +26,37 @@ contract CustomToken is ERC20, Ownable, Pausable {
         uint256 totalAmount;
     }
 
+    mapping(address => bool) private isBlacklisted;
+    mapping(address => uint256) private _transferAllowedAt;
+    mapping(address => bool) private _frozenWallets;
+    mapping(address => bool) private _whitelistedWallets;
+    mapping(address => VestingSchedule) private _vestingSchedules;
+    mapping(address => bool) public signers;
+
     constructor(
         string memory _name,
         string memory _symbol,
         uint8 _decimals,
-        uint256 _initialSupply
+        uint256 _initialSupply,
+        address _uniswapRouter,
+        address _liquidityPair,
+        address[] memory _initialSigners,
+        uint256 _requiredSignatures
     ) ERC20(_name, _symbol) {
         _owner = msg.sender;
         _mint(msg.sender, _initialSupply * (10 ** uint256(_decimals)));
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        liquidityPair = _liquidityPair;
+        for (uint256 i = 0; i < _initialSigners.length; i++) {
+            signers[_initialSigners[i]] = true;
+        }
+
+        requiredSignatures = _requiredSignatures;
+    }
+
+    modifier onlySigner() {
+        require(signers[msg.sender], "Sender is not a signer");
+        _;
     }
 
     function mint(address account, uint256 amount) public onlyOwner {
@@ -137,5 +158,61 @@ contract CustomToken is ERC20, Ownable, Pausable {
         );
         _transferAllowedAt[msg.sender] = block.timestamp + transferDelay;
         return super.transfer(recipient, amount);
+    }
+
+    // Set the Uniswap router and liquidity pair addresses
+    function setUniswapRouterAndPair(
+        address _uniswapRouter,
+        address _liquidityPair
+    ) external onlyOwner {
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        liquidityPair = _liquidityPair;
+    }
+
+    // Lock a specific amount of tokens as liquidity
+    function lockLiquidity(uint256 amount) external onlySigner {
+        require(liquidityPair != address(0), "Liquidity pair address not set");
+
+        // Approve the Uniswap router to spend the token
+        _approve(address(this), address(uniswapRouter), amount);
+
+        // Add liquidity to Uniswap
+        uniswapRouter.addLiquidityETH{value: address(this).balance}(
+            address(this),
+            amount,
+            0,
+            0,
+            owner(),
+            block.timestamp + 3600
+        );
+    }
+
+    function deposit(uint256 amount) external onlyOwner {
+        _mint(address(this), amount);
+    }
+
+    // Auto liquidity mechanism
+    function _autoLiquidity(uint256 amount) internal {
+        require(liquidityPair != address(0), "Liquidity pair address not set");
+
+        // Split the amount for recipient and liquidity
+        uint256 liquidityAmount = amount.div(2);
+        uint256 recipientAmount = amount.sub(liquidityAmount);
+
+        // Approve the Uniswap router to spend the token
+        _approve(address(this), address(uniswapRouter), liquidityAmount);
+
+        // Add liquidity to Uniswap
+        uniswapRouter.addLiquidityETH{value: address(this).balance}(
+            address(this),
+            liquidityAmount,
+            0,
+            0,
+            owner(),
+            block.timestamp + 3600
+        );
+
+        // Transfer the remaining tokens to the recipient
+        _transfer(address(this), msg.sender, recipientAmount);
     }
 }
